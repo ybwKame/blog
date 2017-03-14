d823badbe283a767a5c52b742aa601592e073389
kame <929994814@qq.com>
2017-03-14 15:41:50 +0800

    修改了ueditor配置，测试发日志功能成功

--- Home/Controller/UserController.class.php
+++ Home/Controller/UserController.class.php
@@ -5,6 +5,11 @@ class UserController extends Controller{
 	public function index(){
 		$blog = D('blog');
 		$data = $blog->field("title,content,category,label,author,add_time")->order("add_time desc")->select();
+		foreach ($data as $k => $v) {
+			# code...
+			$data[$k]['add_time'] = date("Y-m-d H:i:s",$v['add_time']);
+			$data[$k]['content'] = html_entity_decode($v['content']);
+		}
 		$this->assign("data",$data);
 		$this->display();
 	}
