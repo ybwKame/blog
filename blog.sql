6f6a2ac18817966f5bdbe9189b73c5ee64d83c64
kame <929994814@qq.com>
2017-03-14 13:48:45 +0800

    添加了日志发布功能

--- blog.sql
+++ blog.sql
@@ -9,5 +9,7 @@ content varchar(500) not null default '',
 comment varchar(50) not null default '',
 category varchar(30) not null default '',
 label varchar(30) not null default '',
-author varchar(20) not null default ''
+author varchar(20) not null default '',
+add_time int(15) not null default 0,
+mod_time int(15) not null default 0
 )engine myisam charset utf8;
